192fffb0d644b13c69fb36ace6fb475f4bd9bf82
Juan Vesga <jfv09@dide.local>
2019-05-13 18:00:15 +0100

    Bar plots of remote Inc

--- main_script.R
+++ main_script.R
@@ -1,5 +1,5 @@
- # install.packages("deSolve", dep=TRUE)
- # install.packages("gridExtra", dep = TRUE)
+# install.packages("deSolve", dep=TRUE)
+# install.packages("gridExtra", dep = TRUE)
 rm(list=ls())
 # dev.off()
 
@@ -92,12 +92,12 @@ params_new["selfcure"]<-1/(T.durat*0.5)
 
 # Starting conditions
 xstart <- c(U = tail(sfin$U,1), 
-             L = tail(sfin$L,1),  
-             I = tail(sfin$I,1), 
-             R = tail(sfin$R,1),
-             Incidence= tail(sfin$Incidence,1),
-             Irecent= tail(sfin$Irecent,1),  
-             Iremote= tail(sfin$Iremote,1)) 
+            L = tail(sfin$L,1),  
+            I = tail(sfin$I,1), 
+            R = tail(sfin$R,1),
+            Incidence= tail(sfin$Incidence,1),
+            Irecent= tail(sfin$Irecent,1),  
+            Iremote= tail(sfin$Iremote,1)) 
 
 
 #Create function handle
@@ -110,6 +110,7 @@ out2 <- as.data.frame(ode(y = xstart, times = times_new,
 N <- out2$U+out2$L+out2$I+out2$R  
 
 rate.inc2<- 1e5*(diff(out2$Incidence)/N[1:length(N)-1])
+fr.remo2 <- diff(out2$Iremote)/diff(out2$Incidence)
 time2<-out2$time[1:length(out2$time)-1]
 plot(time2, rate.inc2, col='blue',type='l',ylim = c(0,max(rate.inc2)),
      xlab ='Years', ylab = 'TB Incidence x 100K')
@@ -139,6 +140,7 @@ out3 <- as.data.frame(ode(y = xstart, times = times_new,
 N <- out3$U+out3$L+out3$I+out3$R  
 
 rate.inc3<- 1e5*(diff(out3$Incidence)/N[1:length(N)-1])
+fr.remo3 <- diff(out3$Iremote)/diff(out3$Incidence)
 time3<-out3$time[1:length(out3$time)-1]
 plot(time3, rate.inc3, col='blue',type='l',ylim = c(0,max(rate.inc3)),
      xlab ='Years', ylab = 'TB Incidence x 100K')
@@ -169,6 +171,7 @@ out4 <- as.data.frame(ode(y = xstart, times = times_new,
 N <- out4$U+out4$L+out4$I+out4$R  
 
 rate.inc4<- 1e5*(diff(out4$Incidence)/N[1:length(N)-1])
+fr.remo4 <- diff(out4$Iremote)/diff(out4$Incidence)
 time4<-out4$time[1:length(out4$time)-1]
 plot(time4, rate.inc4, col='blue',type='l',ylim = c(0,max(rate.inc4)),
      xlab ='Years', ylab = 'TB Incidence x 100K')
@@ -188,15 +191,14 @@ out0 <- as.data.frame(ode(y = xstart, times = times_new,
 
 N <- out0$U+out0$L+out0$I+out0$R  
 rate.inc0<- 1e5*(diff(out0$Incidence)/N[1:length(N)-1])
-
+fr.remo0 <- diff(out0$Iremote)/diff(out0$Incidence)
 
 ############################################
 ############################## Plot all
 
+# Trajectories
 Years<-time2+1617
-
 allruns<-data.frame(Years , rate.inc0, rate.inc2, rate.inc3, rate.inc4)
-
 sz<-1.2
 p<- ggplot(data=allruns, mapping = aes(x=Years, y=value))
 
@@ -205,18 +207,43 @@ p1<-p +
   geom_line(aes(y=rate.inc2, colour="Treatment"),  size=sz)+
   geom_line(aes(y=rate.inc3, colour="Transmission"),  size=sz)+
   geom_line(aes(y=rate.inc4, colour="LTBI"), size=sz)+
-  scale_colour_manual("", 
+  scale_colour_manual(name="Intervention",
+                      breaks=c("Baseline","Treatment","Transmission","LTBI"), 
                       values = c("Baseline"="indianred2", "Treatment"="yellow3", 
                                  "Transmission"="springgreen4", "LTBI"="royalblue")) +
   geom_hline(yintercept=rate.inc0[1]*0.1, linetype="dashed", color = "black") +
-    ylim(0,max(rate.inc0))+
+  ylim(0,max(rate.inc0))+
   ggtitle ('TB Interventions') +
   theme_bw() + ylab('Rate per 100,000')
-  
- grid.arrange(p1, ncol=1)
 
 
+# Bars
+remoteI<-data.frame(
+  Intervention=factor(c("Baseline","Treatment","Transmission","LTBI"),
+                      levels=c("Baseline","Treatment","Transmission","LTBI")),
+  remote=c(tail(fr.remo0, n=1), tail(fr.remo2, n=1),
+           tail(fr.remo3, n=1) , tail(fr.remo4, n=1)) )
+
+remoteI$remote<-remoteI$remote*100
+
+b<- ggplot(data=remoteI, mapping=aes(x=Intervention, y=remote, fill=Intervention))
+p2<- b +
+  geom_bar(stat="identity")+
+  scale_fill_manual(values = c("Baseline"="indianred2", 
+                               "Treatment"="yellow3", 
+                               "Transmission"="springgreen4", 
+                               "LTBI"="royalblue")) +
+  ylab('Fraction Remote') +
+  ggtitle ('Incidence from remote source') +
+  theme_bw()
   
+  
+
+
+grid.arrange(p1,p2, ncol=1, nrow=2)
+
+
+
 # plot(time2, rate.inc0, col='blue',type='l',ylim = c(0,max(rate.inc0)),
 #      xlab ='Years', ylab = 'TB Incidence x 100K')
 # lines(time2, rate.inc2, col='black')
