3d49bb6f882bae7d5743246cbd12605b0f6e888b
Juan Vesga <jfv09@dide.local>
2019-05-15 17:36:03 +0100

    RMarkdwon draft & new plots

--- main_script.R
+++ main_script.R
@@ -13,29 +13,31 @@ library(ggplot2)
 source("fun/TB.Basic.R")
 source("fun/scale_up.R")
 
+#-----------------------------------------------------------------------
+#1.a) Given lecture and previous statement complete the table of parameters 
+
+#1.b) looking at the resulting parameters could you infer what is the average
+#duration of the infectious period? 
+#-----------------------------------------------------------------------
 
 # Model Parameters
-T.lat    <- 67              # latency
-T.durat  <- 6.5             # duration of infectious period
-T.lxpc   <- 67              # Life expec
-T.tbout  <- 6.5             # mort Tb untreated
-
-phi      <- 0.1*(1/T.lat)  # 10% of slow latent progress over a lifetime
-selfcure <- 1/T.durat       # TB cured spontaneusly
-mu       <- 1/T.lxpc        # Background mortality
-mu_tb    <- 1/T.tbout       # TB mortality
-beta     <- 3              # Transmission rate per capita
-fast     <- 0.14            # fraction fast progressing to active TB
+T.lfx    <- 72              # life expectancy
+phi      <- 0.1*(1/T.lfx)   # 10% of slow latent progress over a lifetime
+selfcure <- 0.5*(1/3)       # TB cured spontaneusly
+mu       <- 1/T.lfx         # Background mortality
+mu_tb    <- 0.5*(1/3)       # TB mortality
+beta     <- 6.5             # Transmission rate per capita
+fast     <- 0.1             # fraction fast progressing to active TB
 imm      <- 0.5             # Infectiousness decline (partial immunity)
-relapse  <- 0.0032           # Relapse rate
+relapse  <- 0.005           # Relapse rate
+q        <- 0               # immunized
 
-
-t.intervention <- 400      # ??
-t.scale        <- 3        # Scaling up time
+t.intervention <- 400      # years of simulation
+t.scale        <- 3        # Scaling up time of interventions
 
 # Initial Conditions
-N <- 1                  # ??
-I0 <-1e-6               # ??
+N <- 1                  # Total population equal to unity 
+I0 <-1e-6               # TB seed at time 0
 
 #Prepare tu run
 params <- c(phi=phi, selfcure=selfcure, mu=mu, mu_tb=mu_tb
@@ -57,25 +59,125 @@ xstart <- c(U = N-I0,
 out <- as.data.frame(ode(y = xstart, times = times, 
                          func = TB.Basic, parms = params))   #
 
-
-#Plot some results
-plot(out$time, out$U, col='blue',type='l',ylim = c(0,max(out$U)),
-     xlab ='Years', ylab = 'Number')
-lines(out$time, out$L, col='black')
-lines(out$time, out$I, col='red')
-lines(out$time, out$R, col='green')
-
-legend('topleft',c('U','L','I','R'),lwd=2,col=c('blue','black','red','green'))
-
-
-N <- out$U+out$L+out$I+out$R  
-
+# Model output
+N       <- out$U+out$L+out$I+out$R  
 rate.inc<- 1e5*(diff(out$Incidence)/N[1:length(N)-1])
-time<-out$time[1:length(out$time)-1]
-plot(time, rate.inc, col='blue',type='l',ylim = c(0,max(rate.inc)),
-     xlab ='Years', ylab = 'TB Incidence x 100K')
+fr.remo <- diff(out$Iremote)/diff(out$Incidence)
+time    <- out$time[1:length(out$time)-1]
+
+# Order in dataframe for plotting
+dat<-data.frame(Years=time+(2019-400), incidence=rate.inc)
+
+#Create plot
+p<- ggplot(data=dat, mapping = aes(x=Years, y=incidence))
+p + 
+  geom_line(col="blue",  size=1.2) +
+  ggtitle ('TB Incidence') +
+  theme_bw() + ylab('Rate per 100,000 pop')
 
 
+#-----------------------------------------------------------------------
+#2) Can you modify manually the transmission rate per capita to achieve an
+# incidence rate as that of Bangladesh in 2017?  
+#-----------------------------------------------------------------------
+
+
+Inc.Bangladesh<- 221
+
+p1<-p + 
+  geom_line(col="blue",  size=1.2) +
+  ggtitle ('TB Incidence') +
+  theme_bw() + ylab('Rate per 100,000 pop') +
+  geom_point(aes(x=2017, y=Inc.Bangladesh), colour="red", size=6, shape=18) 
+  
+
+# Pie of remote incidence in 2017
+df <- data.frame(
+  Source = c("Recent", "Remote"),
+  value  = c(1-tail(fr.remo,1),tail(fr.remo,1))
+)
+
+mycols <- c("#0073C2FF", "#EFC000FF")
+pie<- ggplot(df, aes(x="", y=value, fill=Source))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0)+
+  scale_fill_manual(values = mycols) +
+  theme_void()
+
+
+grid.arrange(p1,pie, ncol=2, nrow =2)
+
+
+########################################################################
+## Simulation 1
+# An Intervention simulating introduction of treatment
+
+# Prepare for new simulation
+sfin       <- tail(out,1)     
+params_new <- params
+params_old <- params
+times_new  <- seq(t.intervention, t.intervention+25 , by=1)
+t.interv   <- c(times_new[2], times_new[2]+t.scale)
+
+# Change parameters for intervention
+T.cs <-   1   # Time delay (yrs) between developing symptoms and seeking for care
+pDx  <-   0.5 # Probability of being diagnosed once sought care
+pTx  <-   0.7 # probability of recieving correct Tx if diagnosed
+T.rTx<-   0.5 # 6 months treatment duration
+
+Tx <-pDx*pTx*(1/(T.cs+T.rTx))
+
+params_new["selfcure"]<-selfcure + Tx
+
+# Starting conditions
+xstart <- c(U = sfin$U, 
+            L = sfin$L,  
+            I = sfin$I, 
+            R = sfin$R,
+            Incidence= sfin$Incidence,
+            Irecent=   sfin$Irecent,  
+            Iremote=   sfin$Iremote) 
+
+
+#Create function handle
+fx<-TB.Basic
+scalefx<-function(t, state, parameters) scale_up(t, state, parameters,t.interv,params,fx)
+
+#Run the model
+out2 <- as.data.frame(ode(y = xstart, times = times_new, 
+                          func = scalefx, parms = params_new))  # ??
+# Model output
+N         <- out2$U+out2$L+out2$I+out2$R  
+rate.inc2 <- 1e5*(diff(out2$Incidence)/N[1:length(N)-1])
+fr.remo2 <- diff(out2$Iremote)/diff(out2$Incidence)
+time      <- out2$time[1:length(out2$time)-1]
+dat<-data.frame(Years=time+(2019-400), incidence=rate.inc2)
+
+# Create plot
+p<- ggplot(data=dat, mapping = aes(x=Years, y=incidence))
+
+p1<-p + 
+  geom_line(col="blue",  size=1.2) +
+  ggtitle ('TB Incidence') +
+  theme_bw() + ylab('Rate per 100,000 pop')+
+  ylim(0,max(rate.inc2))
+
+
+# Pie chart of remote vs recent incidence 
+df <- data.frame(
+  Source = c("Recent", "Remote"),
+  value  = c(1-tail(fr.remo2,1),tail(fr.remo2,1))
+)
+
+mycols <- c("#0073C2FF", "#EFC000FF")
+pie<- ggplot(df, aes(x="", y=value, fill=Source))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0)+
+  scale_fill_manual(values = mycols) +
+  theme_void()
+
+
+grid.arrange(p1,pie, ncol=2, nrow =2)
 ########################################################################
 ## Simulation 2
 # An Intervention simulating introduction of treatment
